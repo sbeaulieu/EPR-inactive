1ecbc86746aaebd35f77bef2077c06fc74a28c7f
Stace Beaulieu <stace@whoi.edu>
2025-04-22 17:17:10 -0400

    added output per rock

--- long_to_wide_macrofauna_AT50-33.R
+++ long_to_wide_macrofauna_AT50-33.R
@@ -28,27 +28,51 @@ input_subset <- select(input, 'Alvin Dive ID', 'Rock association', 'Event ID (AL
 
 # note there is 1 Melanodrymia sp. that was lost so...
 # once I subset rows to Morphotype not NA, need to save subsample plus NA
+# and Morphotypes to exclude
 input_subset <- filter(input_subset, !is.na(Morphotype))
 input_subset <- filter(input_subset, Morphotype != "bulk")
 input_subset$Bulk_or_Subsample <- input_subset$`Bulk or Subsample`
 input_subset <- filter(input_subset, Bulk_or_Subsample != "bulk"|is.na(Bulk_or_Subsample))
+input_subset <- filter(input_subset, Morphotype != "tissue" & Morphotype != "rock")
 
 input_subset$eventID <- input_subset$`Event ID (AL####-R#-S/R/W)`
+# hard-coding to harmonize 
 input_subset$Morphotype <- gsub("Neolepetopsis.*", "Neolepetopsis", input_subset$Morphotype)
+input_subset <- input_subset %>% 
+  mutate(Morphotype = replace(Morphotype, Morphotype == "Protist", "cauliflower protist"))
 # in case capitalization matters for group_by
 input_subset$Morphotype <- tolower(input_subset$Morphotype)
 
+# per eventID
 input_subset_eventID <- select(input_subset, eventID, Morphotype, Count)
 # group by eventID then by morphotype
 input_subset_eventID_grouped <- input_subset_eventID %>%
   group_by(eventID, Morphotype) %>%
   summarise(Total = sum(Count))
 
-# pivot from long to wide
+# pivot per eventID from long to wide
 wide_eventID <- pivot_wider(
   input_subset_eventID_grouped,
   id_cols = Morphotype,
   names_from = eventID,
   values_from = Total
 )
-# write.csv(wide_eventID,"wide_eventID_At-Sea_AT50-33.csv")
+# write.csv(wide_eventID,"wide_per_eventID_At-Sea_AT50-33.csv")
+
+# per rock
+input_subset_rock <- unite(input_subset, Dive_Rock, `Alvin Dive ID`, `Rock association`, sep = "_", remove = TRUE, na.rm = FALSE)
+input_subset_rock <- select(input_subset_rock, Dive_Rock, Morphotype, Count)
+
+# group by rock then by morphotype
+input_subset_rock_grouped <- input_subset_rock %>%
+  group_by(Dive_Rock, Morphotype) %>%
+  summarise(Total = sum(Count))
+
+# pivot per rock from long to wide
+wide_rock <- pivot_wider(
+  input_subset_rock_grouped,
+  id_cols = Morphotype,
+  names_from = Dive_Rock,
+  values_from = Total
+)
+# write.csv(wide_rock,"wide_per_rock_At-Sea_AT50-33.csv")

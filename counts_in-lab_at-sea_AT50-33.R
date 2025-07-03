2a311fc484a3358416be9423c7f9f66143d14706
Stace Beaulieu <stace@whoi.edu>
2025-07-03 10:52:54 -0400

    joined with AB in-lab counts

--- counts_in-lab_at-sea_AT50-33.R
+++ counts_in-lab_at-sea_AT50-33.R
@@ -1,6 +1,6 @@
 # script to integrate in-lab with at-sea counts
 # Stace Beaulieu 
-# 2025-07-02
+# 2025-07-03
 
 #Load required packages
 
@@ -20,13 +20,19 @@ input_AtSea <-readxl::read_xlsx("macrofauna_AT50-33_At-sea_per_eventID_20250626.
 # date downloaded in-lab WORKING COPY
 input_InLab_EB <-readxl::read_xlsx("macrofauna_AT50-33_in-lab_Bogomolni_20250702.xlsx", skip=3)
 input_InLab_WH <-readxl::read_xlsx("macrofauna_AT50-33_in-lab_Hamlin_20250702.xlsx", skip=3)
-# add line to read in AB's in-lab AT50-33
+# also read in AB's in-lab AT50-33
+input_InLab_AB <-readxl::read_xlsx("AT50-33_Macrofauna_Counts_Best_20250703.xlsx", skip=9)
 
-#Join on Morphotype column
+#Join in-lab with at-sea counts
+# Join InLab EB and WH on Morphotype column
 input_joined_EB <- dplyr::full_join(input_AtSea, input_InLab_EB, c("Morphotype" = "Morphotype_AT50-33_At-sea_20250626"))
-input_joined_EB_WH <- dplyr::full_join(input_joined_EB, input_InLab_WH, c("Morphotype" = "Morphotype_AT50-33_At-sea_20250626"))
+# note that EB and WH sheets also include category_in_Ayinde_Best_template 
+input_joined_EB_WH <- dplyr::full_join(input_joined_EB, input_InLab_WH, c("Morphotype" = "Morphotype_AT50-33_At-sea_20250626", "category_in_Ayinde_Best_template" = "category_in_Ayinde_Best_template"))
+# Join InLab AB on category AB (= Morphotype DSR) column
+input_joined_EB_WH_AB <- dplyr::full_join(input_joined_EB_WH, input_InLab_AB, c("category_in_Ayinde_Best_template" = "Morphotype DSR"))
+# note that AB columns are type chr
 
 #Select a specific rock to check counts
 rock_prefix <- "AL5288-R01"
-subset <- dplyr::select(input_joined_EB_WH, 'revised template', Morphotype, starts_with(rock_prefix))
+subset <- dplyr::select(input_joined_EB_WH_AB, 'revised template', Morphotype, starts_with(rock_prefix))
 # write.csv(subset, file = paste(rock_prefix,".csv",sep=""), row.names = FALSE)

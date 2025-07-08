cc8e2ddc655b118fb3f5d47a68453024e0ae41d2
Stace Beaulieu <stace@whoi.edu>
2025-07-08 15:16:25 -0400

    group_by high_taxon_rank

--- counts_in-lab_at-sea_AT50-33.R
+++ counts_in-lab_at-sea_AT50-33.R
@@ -1,6 +1,6 @@
 # script to integrate in-lab with at-sea counts
 # Stace Beaulieu 
-# 2025-07-07
+# 2025-07-08
 
 #Load required packages
 
@@ -18,8 +18,8 @@ setwd("/Users/sbeaulieu/Downloads")
 # authoritative at-sea sheet
 input_AtSea <-readxl::read_xlsx("macrofauna_AT50-33_At-sea_per_eventID_20250626.xlsx", skip=1)
 # date downloaded in-lab WORKING COPY
-input_InLab_EB <-readxl::read_xlsx("macrofauna_AT50-33_in-lab_Bogomolni_20250702.xlsx", skip=3)
-input_InLab_WH <-readxl::read_xlsx("macrofauna_AT50-33_in-lab_Hamlin_20250702.xlsx", skip=3)
+input_InLab_EB <-readxl::read_xlsx("macrofauna_AT50-33_in-lab_Bogomolni_20250708.xlsx", skip=3)
+input_InLab_WH <-readxl::read_xlsx("macrofauna_AT50-33_in-lab_Hamlin_20250708.xlsx", skip=3)
 # also read in AB's in-lab AT50-33
 # note that AB columns are type chr due to Present and Absent
 input_InLab_AB <-readxl::read_xlsx("AT50-33_Macrofauna_Counts_Best_20250703.xlsx", skip=9)
@@ -39,12 +39,19 @@ InLab_AB_numeric <- input_InLab_AB %>%
 # Join InLab EB and WH on Morphotype column
 input_joined_EB <- dplyr::full_join(input_AtSea, input_InLab_EB, c("Morphotype" = "Morphotype_AT50-33_At-sea_20250626"))
 # note that EB and WH sheets also include category_in_Ayinde_Best_template 
-input_joined_EB_WH <- dplyr::full_join(input_joined_EB, input_InLab_WH, c("Morphotype" = "Morphotype_AT50-33_At-sea_20250626", "category_in_Ayinde_Best_template" = "category_in_Ayinde_Best_template"))
+# note that EB and WH sheets also include high_taxon_rank 
+input_joined_EB_WH <- dplyr::full_join(input_joined_EB, input_InLab_WH, c("Morphotype" = "Morphotype_AT50-33_At-sea_20250626", "category_in_Ayinde_Best_template" = "category_in_Ayinde_Best_template", "high_taxon_rank" = "high_taxon_rank"))
 # Join InLab AB on category AB (= Morphotype DSR) column
 input_joined_EB_WH_AB <- dplyr::full_join(input_joined_EB_WH, InLab_AB_numeric, c("category_in_Ayinde_Best_template" = "Morphotype DSR"))
-
+# add here: write.csv
+# consider adding top-most rows for Feature and Rock Type
 
 #Select a specific rock to check counts
 rock_prefix <- "AL5288-R01"
-subset <- dplyr::select(input_joined_EB_WH_AB, 'revised template', Morphotype, starts_with(rock_prefix))
+subset <- dplyr::select(input_joined_EB_WH_AB, high_taxon_rank, 'revised template', Morphotype, starts_with(rock_prefix))
 # write.csv(subset, file = paste(rock_prefix,".csv",sep=""), row.names = FALSE)
+# group by column high_taxon_rank
+subset_grouped_taxon <- subset |>
+  group_by(high_taxon_rank) |>
+  summarise_at(vars(starts_with(rock_prefix)), sum, na.rm = TRUE) # AB will have NAs
+# need to specify the order for rows

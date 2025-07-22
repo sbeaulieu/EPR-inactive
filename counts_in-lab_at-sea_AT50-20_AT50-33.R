9921fdd90a37e54f6a17dfc96c1377cb5359d02a
Stace Beaulieu <stace@whoi.edu>
2025-07-22 12:51:16 -0400

    join AT50-33 with AT50-20 counts

--- counts_in-lab_at-sea_AT50-20_AT50-33.R
+++ counts_in-lab_at-sea_AT50-20_AT50-33.R
@@ -1,6 +1,7 @@
 # script to integrate in-lab with at-sea counts
+# cruises AT50-20 and AT50-33
 # Stace Beaulieu, Emmanuelle Bogomolni
-# 2025-07-16
+# 2025-07-22
 
 #Load required packages
 
@@ -16,7 +17,7 @@ setwd("/Users/sbeaulieu/Downloads")
 # download from project Google Drive
 # by default readxl reads in the first sheet/tab of the EXCEL file
 
-# authoritative at-sea sheet
+# authoritative at-sea sheet AT50-33
 input_AtSea <-readxl::read_xlsx("macrofauna_AT50-33_At-sea_per_eventID_20250626.xlsx", skip=1)
 # date downloaded in-lab WORKING COPY
 input_InLab_EB <-readxl::read_xlsx("macrofauna_AT50-33_in-lab_Bogomolni_20250714.xlsx", skip=3)
@@ -34,11 +35,31 @@ InLab_AB_numeric <- input_InLab_AB %>%
     )
   )
 )
-# also read in sampling events from rock log
+# also read in sampling events from rock log AT50-33
 # to be able to add feature and rock type to plots
 input_rock_log <- read.csv("../Documents/QGIS_project_EPR_inactive_AT50-33/sampling_events_from_rock_log_AT50-33_20250709.csv")
 
-#Join in-lab with at-sea counts
+#Loading in the AT50-20 datasheets
+# also need to add code and/or read in sheet for rock type 
+Snails_Harris<-readxl::read_xlsx("AT50-20_Macrofauna_Counts_Harris_HARMONIZED_20250722.xlsx", skip = 9)
+Snails_Best<-readxl::read_xlsx("AT50-20_Macrofauna_Counts_Best_20250722.xlsx", skip = 9)
+
+#Combining Snails_Best and Snails_Harris using the join function
+#these include at-sea and in-lab
+Snails_AT5020<-full_join(Snails_Best, Snails_Harris, by="...2")
+# note that MH and AB columns are type chr due to Present and Absent
+Snails_AT5020_numeric <- Snails_AT5020 %>%
+  mutate(
+    across(starts_with('AL'),
+           function(x) case_when(
+             x == "Present" ~ 1,
+             x == "Absent" ~ 0,
+             TRUE ~ as.numeric(x)
+           )
+    )
+  )
+
+#AT50-33 Join in-lab with at-sea counts
 # Join InLab EB and WH on Morphotype column
 input_joined_EB <- dplyr::full_join(input_AtSea, input_InLab_EB, c("Morphotype" = "Morphotype_AT50-33_At-sea_20250626"))
 # note that EB and WH sheets also include category_in_Ayinde_Best_template 
@@ -46,67 +67,69 @@ input_joined_EB <- dplyr::full_join(input_AtSea, input_InLab_EB, c("Morphotype" 
 input_joined_EB_WH <- dplyr::full_join(input_joined_EB, input_InLab_WH, c("Morphotype" = "Morphotype_AT50-33_At-sea_20250626", "category_in_Ayinde_Best_template" = "category_in_Ayinde_Best_template", "high_taxon_rank" = "high_taxon_rank"))
 # Join InLab AB on category AB (= Morphotype DSR) column
 input_joined_EB_WH_AB <- dplyr::full_join(input_joined_EB_WH, InLab_AB_numeric, c("category_in_Ayinde_Best_template" = "Morphotype DSR"))
-# add here: write.csv
-# consider adding top-most rows for Feature and Rock Type
 
-#Select a specific rock to check counts
-rock_prefix <- "AL5292-R02"
-subset <- dplyr::select(input_joined_EB_WH_AB, high_taxon_rank, 'revised template', Morphotype, starts_with(rock_prefix))
-# write.csv(subset, file = paste(rock_prefix,".csv",sep=""), row.names = FALSE)
-# group by column high_taxon_rank
-subset_grouped_taxon <- subset |>
-  group_by(high_taxon_rank) |>
-  summarise_at(vars(starts_with(rock_prefix)), sum, na.rm = TRUE) # AB will have NAs
-# need to specify the order for rows here or when plotting
+# join AT50-20 with AT50-33
+All_Combined<-full_join(input_joined_EB_WH_AB, Snails_AT5020_numeric, by = c("category_in_Ayinde_Best_template" ="...2" ))
 
-# prototype plot for taxon count totals 1 rock
-subset_grouped_taxon_total <- mutate(subset_grouped_taxon, total = rowSums(across(where(is.numeric))))
-subset_grouped_taxon_total$'Sample.I.D.' <- rock_prefix # to be able to join with rock log
-joined_one_rock <- left_join(subset_grouped_taxon_total, input_rock_log, by = 'Sample.I.D.')
-joined_one_rock |>
-  ggplot(aes(x = high_taxon_rank, y = total, fill = Feature)) +
-  geom_col(position = "dodge") +
-  labs(
-    title = "Taxon Count Totals per Rock by Feature",
-    x = "Taxon", y = "Total Count", fill = "Feature"
-  ) +
-  theme_minimal() +
-  theme(
-    axis.text.x = element_text(angle = 45, hjust = 1),
-    legend.position = "top"
-  )
+# #Select a specific rock to check counts
+# rock_prefix <- "AL5292-R02"
+# subset <- dplyr::select(input_joined_EB_WH_AB, high_taxon_rank, 'revised template', Morphotype, starts_with(rock_prefix))
+# # write.csv(subset, file = paste(rock_prefix,".csv",sep=""), row.names = FALSE)
+# # group by column high_taxon_rank
+# subset_grouped_taxon <- subset |>
+#   group_by(high_taxon_rank) |>
+#   summarise_at(vars(starts_with(rock_prefix)), sum, na.rm = TRUE) # AB will have NAs
+# # need to specify the order for rows here or when plotting
+# 
+# # prototype plot for taxon count totals 1 rock
+# subset_grouped_taxon_total <- mutate(subset_grouped_taxon, total = rowSums(across(where(is.numeric))))
+# subset_grouped_taxon_total$'Sample.I.D.' <- rock_prefix # to be able to join with rock log
+# joined_one_rock <- left_join(subset_grouped_taxon_total, input_rock_log, by = 'Sample.I.D.')
+# joined_one_rock |>
+#   ggplot(aes(x = high_taxon_rank, y = total, fill = Feature)) +
+#   geom_col(position = "dodge") +
+#   labs(
+#     title = "Taxon Count Totals per Rock by Feature",
+#     x = "Taxon", y = "Total Count", fill = "Feature"
+#   ) +
+#   theme_minimal() +
+#   theme(
+#     axis.text.x = element_text(angle = 45, hjust = 1),
+#     legend.position = "top"
+#   )
 
-# repeating to see if we can plot another rock with same structure to data frame
-#Select a specific rock to check counts
-rock_prefix2 <- "AL5294-R02"
-subset2 <- dplyr::select(input_joined_EB_WH_AB, high_taxon_rank, 'revised template', Morphotype, starts_with(rock_prefix2))
-# write.csv(subset, file = paste(rock_prefix,".csv",sep=""), row.names = FALSE)
-# group by column high_taxon_rank
-subset2_grouped_taxon <- subset2 |>
-  group_by(high_taxon_rank) |>
-  summarise_at(vars(starts_with(rock_prefix2)), sum, na.rm = TRUE) # AB will have NAs
-subset2_grouped_taxon_total <- mutate(subset2_grouped_taxon, total = rowSums(across(where(is.numeric))))
-subset2_grouped_taxon_total$'Sample.I.D.' <- rock_prefix2 # to be able to join with rock log
-joined2_one_rock <- left_join(subset2_grouped_taxon_total, input_rock_log, by = 'Sample.I.D.')
+# # repeating to see if we can plot another rock with same structure to data frame
+# #Select a specific rock to check counts
+# rock_prefix2 <- "AL5294-R02"
+# subset2 <- dplyr::select(input_joined_EB_WH_AB, high_taxon_rank, 'revised template', Morphotype, starts_with(rock_prefix2))
+# # write.csv(subset, file = paste(rock_prefix,".csv",sep=""), row.names = FALSE)
+# # group by column high_taxon_rank
+# subset2_grouped_taxon <- subset2 |>
+#   group_by(high_taxon_rank) |>
+#   summarise_at(vars(starts_with(rock_prefix2)), sum, na.rm = TRUE) # AB will have NAs
+# subset2_grouped_taxon_total <- mutate(subset2_grouped_taxon, total = rowSums(across(where(is.numeric))))
+# subset2_grouped_taxon_total$'Sample.I.D.' <- rock_prefix2 # to be able to join with rock log
+# joined2_one_rock <- left_join(subset2_grouped_taxon_total, input_rock_log, by = 'Sample.I.D.')
+# 
+# # prototype plot for taxon count totals 2 rocks
+# # can't row bind until remove the columns specific to rock_prefix
+# joined_one_rock_to_bind <- select(joined_one_rock, high_taxon_rank, total, 'Sample.I.D.', Feature, 'Rock.Type')
+# joined2_one_rock_to_bind <- select(joined2_one_rock, high_taxon_rank, total, 'Sample.I.D.', Feature, 'Rock.Type')
+# two_rocks <- rbind(joined_one_rock_to_bind, joined2_one_rock_to_bind)
+# two_rocks |>
+#   ggplot(aes(x = high_taxon_rank, y = total, fill = Feature)) +
+#   geom_col(position = "dodge") +
+#   labs(
+#     title = "Taxon Count Totals per Rock by Feature",
+#     x = "Taxon", y = "Total Count", fill = "Feature"
+#   ) +
+#   theme_minimal() +
+#   theme(
+#     axis.text.x = element_text(angle = 45, hjust = 1),
+#     legend.position = "top"
+#   )
 
-# prototype plot for taxon count totals 2 rocks
-# can't row bind until remove the columns specific to rock_prefix
-joined_one_rock_to_bind <- select(joined_one_rock, high_taxon_rank, total, 'Sample.I.D.', Feature, 'Rock.Type')
-joined2_one_rock_to_bind <- select(joined2_one_rock, high_taxon_rank, total, 'Sample.I.D.', Feature, 'Rock.Type')
-two_rocks <- rbind(joined_one_rock_to_bind, joined2_one_rock_to_bind)
-two_rocks |>
-  ggplot(aes(x = high_taxon_rank, y = total, fill = Feature)) +
-  geom_col(position = "dodge") +
-  labs(
-    title = "Taxon Count Totals per Rock by Feature",
-    x = "Taxon", y = "Total Count", fill = "Feature"
-  ) +
-  theme_minimal() +
-  theme(
-    axis.text.x = element_text(angle = 45, hjust = 1),
-    legend.position = "top"
-  )
-
+# next block of code for grouped taxon plot
 # initialize data frame to row bind multiple rocks grouped taxon total
 multiple_rocks_grouped_taxon_total <- data.frame(matrix(ncol=5,nrow=0, dimnames=list(NULL, c("high_taxon_rank", "total", "Sample.I.D.", "Feature", "Rock.Type"))))
 
@@ -139,3 +162,24 @@ multiple_rocks_grouped_taxon_total |>
     axis.text.x = element_text(angle = 45, hjust = 1),
     legend.position = "top"
   )
+
+# next block of code ultimately for NMDS 
+# select multiple rocks ultimately for NMDS from both cruises
+rocks_for_nmds <- c("AL5222-SS-R03", "AL5224-LM-R01", "AL5292-R03", "AL5294-R02")
+multiple_rocks_per_event <- dplyr::select(All_Combined, high_taxon_rank, 'revised template', Morphotype, category_in_Ayinde_Best_template, starts_with(rocks_for_nmds))
+
+# initialize data frame to column bind multiple rocks total
+#multiple_rocks_total <- data.frame(matrix(ncol=0,nrow=56))
+multiple_rocks_total <- select(All_Combined, high_taxon_rank, 'revised template', Morphotype, category_in_Ayinde_Best_template)
+
+for (rock_prefix in rocks_for_nmds) {
+  one_rock_total <- multiple_rocks_per_event |>
+    select(starts_with(rock_prefix)) |>
+    mutate(total = rowSums(across(where(is.numeric)))) |>
+    rename(!!rock_prefix := total) |>
+    select(rock_prefix)
+  multiple_rocks_total <- cbind(multiple_rocks_total, one_rock_total)
+}
+
+# write.csv(multiple_rocks_total, file = "multiple_rocks_total_wide.csv", row.names = FALSE)
+

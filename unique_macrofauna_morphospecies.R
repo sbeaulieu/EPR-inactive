7ee3c4c87dfe1c344f7b118b58a566cfcb70e4b5
Stace Beaulieu <stace@whoi.edu>
2026-07-05 19:04:43 -0400

    revised Appendix initiated data table for BCO-DMO

--- unique_macrofauna_morphospecies.R
+++ unique_macrofauna_morphospecies.R
@@ -1,6 +1,7 @@
 # quick script to create Appendix for Photo Guide EPR inactive macrofauna
+# and to create data table for BCO-DMO
 # Stace Beaulieu
-# 2026-06-17
+# 2026-07-05
 
 # input: 
 # download "TEMPLATE_unique_macrofauna_morphospecies_WORKING_COPY" from Google folder as xlsx
@@ -17,13 +18,38 @@ library(readr)
 setwd("C:/Users/sbeaulieu/Downloads")
 
 # load data
-full_data <- read_excel("TEMPLATE_unique_macrofauna_morphospecies_2026-06-17.xlsx", skip = 3)
+full_data <- read_excel("TEMPLATE_unique_macrofauna_morphospecies_2026-07-05.xlsx", skip = 3)
 
 # keep only the unique morphospecies
 uniq_morph <- dplyr::filter(full_data, consider_for_checklist_unique_morphospecies == "y")
 
 # keep only the columns for the Appendix printout
 Appendix_uniq_morph <- dplyr::select(uniq_morph, "Table of Contents", morphospecies, identificationRemarks, kingdom, phylum, class, order, family, genus, species)
+# ultimately decided to not include identificationRemarks in Appendix
 
-# save output file
-readr::write_csv(Appendix_uniq_morph, "Appendix_uniq_morph_2026-06-17.csv", na = "")
+# save output Appendix file
+readr::write_csv(Appendix_uniq_morph, "Appendix_uniq_morph_2026-07-05.csv", na = "")
+
+# keep only the columns for the data table for BCO-DMO
+# exclude deprecated or just used for harmonization
+for_bcodmo <- dplyr::select(full_data, "Table of Contents",
+                            morphospecies,
+                            identificationRemarks,
+                            scientificName,
+                            scientificNameID,
+                            associatedMedia,
+                            occurrenceID,
+                            associatedSequences,
+                            consider_for_checklist_unique_morphospecies,
+                            kingdom,
+                            phylum,
+                            class,
+                            order,
+                            family,
+                            genus,
+                            species)
+
+# save output data table for BCO-DMO
+readr::write_csv(for_bcodmo, "BCO-DMO_macrofauna_morphospecies_2026-07-05.csv", na = "")
+# need to strip the bottom row
+# filter order_unique_morphospecies is not NA
